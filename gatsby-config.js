bcb04b169abcb5d883b8abd108f2f3ced423d60c
Fabian Kresler <fabi.kresler@googlemail.com>
2019-05-21 23:29:45 +0200

    Refactoring Layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,6 +73,7 @@ module.exports = {
       options: {
         pathToConfigModule: `src/utils/typography`
       }
-    }
+    },
+    `gatsby-plugin-styled-components`
   ]
 }
---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledFooter = styled.footer`
+  display: block;
+`
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      © {new Date().getFullYear()}, Built with
+      {` `}
+      <a href="https://www.gatsbyjs.org">Gatsby</a>
+    </StyledFooter>
+  )
+}
+
+export default Footer
